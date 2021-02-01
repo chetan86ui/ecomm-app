7f0d735594d9631dc95f60661f82e59b5409f456
chetan <chetan86ui@gmail.com>
2021-02-01 08:33:20 -0800

    requirment changes

---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ images }}</div>
+</template>
+
+<script>
+export default {
+  name: "ImageCarousel",
+  props: {
+    images: Array
+  },
+  mounted() {
+    console.log("hello", this.images);
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,128 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="product-list">
+      <div
+        class="product"
+        v-for="product in productList"
+        v-bind:key="product.id"
+      >
+        <div class="product-name" v-html="product.name" />
+        <div
+          class="price"
+          v-html="getProductPrice(product.priceRange || product.price)"
+        />
+        <a @click="getcarousel(product.images)">
+          <img :alt="product.hero.alt" :src="product.hero.href" />
+        </a>
+        <ImageCarousel images="product.images" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Vue from "vue";
+import axios from "axios";
+import responseData from "../response.json";
+import ImageCarousel from "@/components/ImageCarousel.vue";
+
+Vue.use(axios);
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    ImageCarousel
+  },
+  data() {
+    return {
+      productList: undefined,
+      response: responseData
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json",
+        {
+          headers: {
+            "Content-Type": "x-www-form-urlencoded"
+          }
+        }
+      )
+      .then(response => {
+        console.log("response", response.data);
+        this.productList = response.data.groups;
+      })
+      .catch(error => {
+        console.warn("Not good man", error);
+        this.productList = this.response.groups;
+        console.log(this.productList);
+      });
+  },
+  methods: {
+    getProductPrice: function(productPrice) {
+      if (productPrice) {
+        let highPrice = productPrice?.selling?.high || "";
+        let lowPrice = productPrice?.selling?.low || "";
+        if (highPrice === "") {
+          highPrice = productPrice?.regular;
+          lowPrice = productPrice?.selling;
+          if (highPrice === lowPrice) {
+            return `$${highPrice}`;
+          } else {
+            return `$${lowPrice}`;
+          }
+        }
+        return `$${lowPrice} - $${highPrice}`;
+      }
+    },
+    getcarousel: function(productImages) {
+      console.log("hello", productImages);
+    }
   }
 };
 </script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.product-list {
+  padding: 0px;
+  margin: 0px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20%, 1fr));
+  grid-gap: 1rem;
+  .product {
+    position: relative;
+    overflow: hidden;
+    .product-name {
+      background: rgba($color: #fff, $alpha: 0.7);
+      color: #666;
+      font-size: 12px;
+      text-transform: uppercase;
+      font-weight: bold;
+      text-align: left;
+      position: absolute;
+      padding: 5px 15px;
+      width: 100%;
+      box-sizing: border-box;
+    }
+    .price {
+      position: absolute;
+      background: #666;
+      color: #fff;
+      padding: 3px 5px;
+      font-weight: 400;
+      bottom: 30px;
+      left: 10px;
+    }
+    a {
+      display: inline-block;
+      width: 100%;
+      cursor: pointer;
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
